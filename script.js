a44cae92facf8ff93417f47d759a8c887409c459
Daniil M. Ozernyi <83814597+DOzernyi@users.noreply.github.com>
2023-12-17 23:58:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flashcard App</title>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap">
-</head>
-<body>
-    <div id="welcome-message">
-        <h1>Welcome to the Flashcard App!</h1>
-        <p>Select a set and start learning.</p>
-    </div>
-    <div id="flashcard-container">
-        <div id="set-selection-container">
-            <label for="flashcard-set-selector">Select Set:</label>
-            <select id="flashcard-set-selector">
-                <option value="set1">Set 1</option>
-                <option value="set2">Set 2</option>
-                <option value="geography">Geography</option>
-            </select>
-        </div>
-        <div id="flashcard">
-            <div id="flashcard-front">Front</div>
-            <div id="flashcard-back">Back</div>
-        </div>
-        <button id="next-button">Next</button>
-        <button id="flip-button">Flip</button>
-    </div>
-    <script src="script.js"></script>
-</body>
-</html>
+document.addEventListener('DOMContentLoaded', function () {
+    // Get elements
+    const flashcardSetSelector = document.getElementById('flashcard-set-selector');
+    const flashcardFront = document.getElementById('flashcard-front');
+    const flashcardBack = document.getElementById('flashcard-back');
+    const nextButton = document.getElementById('next-button');
+    const flipButton = document.getElementById('flip-button');
+
+    // Flashcard sets
+    const flashcardSets = {
+        set1: { front: 'Front 1', back: 'Back 1' },
+        set2: { front: 'Front 2', back: 'Back 2' },
+        geography: { front: 'Geography Front', back: 'Geography Back' }
+    };
+
+    // Initialize flashcard
+    let currentSet = flashcardSets[flashcardSetSelector.value];
+    updateFlashcard(currentSet);
+
+    // Event listeners
+    flashcardSetSelector.addEventListener('change', function () {
+        currentSet = flashcardSets[this.value];
+        updateFlashcard(currentSet);
+    });
+
+    flipButton.addEventListener('click', function () {
+        flipFlashcard();
+    });
+
+    nextButton.addEventListener('click', function () {
+        // Implement logic to move to the next flashcard if needed
+        // For simplicity, it will just flip the current flashcard
+        flipFlashcard();
+    });
+
+    // Function to update flashcard content
+    function updateFlashcard(set) {
+        flashcardFront.textContent = set.front;
+        flashcardBack.textContent = set.back;
+        // Ensure the front is visible when updating the flashcard content
+        flashcardFront.style.transform = 'rotateY(0deg)';
+    }
+
+    // Function to flip the flashcard
+    function flipFlashcard() {
+        flashcardFront.style.transform = 'rotateY(180deg)';
+        flashcardBack.style.transform = 'rotateY(0deg)';
+    }
+});
