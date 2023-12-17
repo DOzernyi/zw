cfc7f5ef7828692d57a496d15dbab0fbeab9622b
Daniil M. Ozernyi <83814597+DOzernyi@users.noreply.github.com>
2023-12-17 17:11:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,49 @@ const flashcard = document.getElementById('flashcard');
 const flashcardFront = document.getElementById('flashcard-front');
 const flashcardBack = document.getElementById('flashcard-back');
 const nextButton = document.getElementById('next-button');
+const flashcardSetSelector = document.getElementById('flashcard-set-selector');
 
-const flashcards = [
-    { front: 'Question 1', back: 'Answer 1' },
-    { front: 'Question 2', back: 'Answer 2' },
-    // Add more flashcards as needed
+const flashcardsSet1 = [
+    { front: 'Question 1 Set 1', back: 'Answer 1 Set 1' },
+    { front: 'Question 2 Set 1', back: 'Answer 2 Set 1' },
+    // Add more flashcards for Set 1 as needed
+];
+
+const flashcardsSet2 = [
+    { front: 'Question 1 Set 2', back: 'Answer 1 Set 2' },
+    { front: 'Question 2 Set 2', back: 'Answer 2 Set 2' },
+    // Add more flashcards for Set 2 as needed
 ];
 
 let currentCardIndex = 0;
+let currentFlashcards = flashcardsSet1; // Default set
 
 function updateFlashcard() {
-    const currentCard = flashcards[currentCardIndex];
+    const currentCard = currentFlashcards[currentCardIndex];
     flashcardFront.innerText = currentCard.front;
-    flashcardBack.innerText = currentCard.back;
+    flashcardBack.innerText = ''; // Clear the back side
 }
 
 function showNextCard() {
-    currentCardIndex = (currentCardIndex + 1) % flashcards.length;
+    currentCardIndex = (currentCardIndex + 1) % currentFlashcards.length;
+    updateFlashcard();
+}
+
+function changeFlashcardSet() {
+    if (flashcardSetSelector.value === 'set1') {
+        currentFlashcards = flashcardsSet1;
+    } else if (flashcardSetSelector.value === 'set2') {
+        currentFlashcards = flashcardsSet2;
+    }
+
+    // Reset card index and update the displayed card
+    currentCardIndex = 0;
     updateFlashcard();
 }
 
 // Initial setup
 updateFlashcard();
 
-// Event listener for the Next button
+// Event listeners
 nextButton.addEventListener('click', showNextCard);
+flashcardSetSelector.addEventListener('change', changeFlashcardSet);
