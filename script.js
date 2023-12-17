eb87182e27999010717d708365db42dc5967c11c
Daniil M. Ozernyi <83814597+DOzernyi@users.noreply.github.com>
2023-12-17 18:58:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,61 +5,54 @@ const nextButton = document.getElementById('next-button');
 const flipButton = document.getElementById('flip-button');
 const flashcardSetSelector = document.getElementById('flashcard-set-selector');
 
-const flashcardsSet1 = [
+const flashcards = {
+  set1: [
     { front: 'Capital of France', back: ['Paris'] },
     { front: 'Largest desert in the world', back: ['Antarctica'] },
     { front: 'Longest river in the world', back: ['Nile'] },
-];
-
-const flashcardsSet2 = [
+  ],
+  set2: [
     { front: 'Question 1 Set 2', back: ['Answer 1 Set 2'] },
     { front: 'Question 2 Set 2', back: ['Answer 2 Set 2'] },
     // Add more flashcards for Set 2 as needed
-];
-
-const flashcardsGeography = [
+  ],
+  geography: [
     { front: 'Capital of Japan', back: ['Tokyo'] },
     { front: 'Highest mountain in the world', back: ['Mount Everest'] },
     { front: 'Country with the most pyramids', back: ['Sudan'] },
-];
+  ],
+};
 
 let currentCardIndex = 0;
-let currentFlashcards = flashcardsSet1; // Default set
-let isShowingFront = true;
+let currentFlashcards = flashcards.set1; // Default set
+
+function updateFlashcard() {
+  const currentCard = currentFlashcards[currentCardIndex];
+  flashcardFront.innerText = isShowingFront ? currentCard.front : '';
+  flashcardBack.innerText = isShowingFront ? '' : currentCard.back.join(' / ');
+  flashcardBack.style.display = isShowingFront ? 'none' : 'flex';
+}
 
 function showFront() {
-    isShowingFront = true;
-    updateFlashcard();
+  isShowingFront = true;
+  updateFlashcard();
 }
 
 function showBack() {
-    isShowingFront = false;
-    updateFlashcard();
-}
-
-function updateFlashcard() {
-    const currentCard = currentFlashcards[currentCardIndex];
-    flashcardFront.innerText = isShowingFront ? currentCard.front : '';
-    flashcardBack.innerText = isShowingFront ? '' : currentCard.back.join(' / ');
+  isShowingFront = false;
+  updateFlashcard();
 }
 
 function showNextCard() {
-    currentCardIndex = (currentCardIndex + 1) % currentFlashcards.length;
-    showFront();
+  currentCardIndex = (currentCardIndex + 1) % currentFlashcards.length;
+  showFront();
 }
 
 function changeFlashcardSet() {
-    if (flashcardSetSelector.value === 'set1') {
-        currentFlashcards = flashcardsSet1;
-    } else if (flashcardSetSelector.value === 'set2') {
-        currentFlashcards = flashcardsSet2;
-    } else if (flashcardSetSelector.value === 'geography') {
-        currentFlashcards = flashcardsGeography;
-    }
-
-    // Reset card index and show the front of the card
-    currentCardIndex = 0;
-    showFront();
+  const selectedSet = flashcardSetSelector.value;
+  currentFlashcards = flashcards[selectedSet] || [];
+  currentCardIndex = 0;
+  showFront();
 }
 
 // Initial setup
